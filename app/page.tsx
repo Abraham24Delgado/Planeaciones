a103d347ec431c6de8305794f0c9376025d55d36
Abraham24Delgado <abraham64delgafo@gmail.com>
2024-09-28 12:39:20 -0600

    Button Login

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+import { ArrowRightIcon } from "@heroicons/react/24/outline";
 export default function Page(){
     return (
             <section className="bg-white dark:bg-green-100 h-screen content-center">
@@ -5,13 +7,14 @@ export default function Page(){
                     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-green-600">Bienvenido a EduPlanner</h1>
                     <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 lg:px-48 dark:text-gray-800">La plataforma diseñada especialmente para ti. Aquí, tu misión de educar y motivar a tus estudiantes se convierte en una experiencia más enriquecedora y eficiente.</p>
                     <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                        <a href="#" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900">
-                            Iniciar sesion
-                            <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                                <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                            </svg>
-                        </a>
-                        <a href="#" className="py-3 px-5 sm:ms-4 text-sm font-medium text-white focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-800">
+                    <Link
+                        href="/login"
+                        className="flex items-center gap-5 self-start rounded-lg bg-green-700 hover:bg-green-800 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-secondary-700 md:text-base"
+                    >
+                        <span>Iniciar sesion</span> <ArrowRightIcon className="w-5 md:w-6" />
+                    </Link>
+                        
+                        <a href="#" className="py-3 px-5 sm:ms-4 text-sm font-medium text-white focus:outline-none bg-gray-700 rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-white focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-700 dark:text-white dark:hover:text-white dark:hover:bg-gray-800">
                             Leer mas
                         </a>  
                     </div>
